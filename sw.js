0f8f00563fff909d2c14440ee87d76d358a54039
jpoon6d23 <jpoon6d23@gmail.com>
2026-06-26 13:58:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='podcast-maker-v29';
+const CACHE='podcast-maker-v30';
 const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
